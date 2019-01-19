93b8a09d39fa031da8d4f0af875b1cfa76fd5ea2
antipa <naantipa@gmail.com>
2019-01-18 17:10:00 -0800

    renamed RandoscopeXCorrMinTF.py to miniscope_utils_tf.py

---
+++ psf_design_nlls.m
@@ -0,0 +1,8 @@
+N = 6;    %Number of planes
+Nl = 20;   %Number of lenslets
+Rmin = 8; % minimum radius
+Rmax = 20;  %Maximum lenslet radius
+cmin = 1/Rmax;  %Curvatures (better for optimization)
+cmax = 1/Rmin;
+
+
