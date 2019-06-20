59968ce06c047abf326aa543183253ba549a281f
kyrollosyanny <kyrollosyanny@gmail.com>
2019-06-20 15:51:00 -0700

    added ability to specify which planes to read from 3D tiff in read_tiff_stack.m

--- read_tiff_stack.m
+++ read_tiff_stack.m
@@ -1,15 +1,25 @@
 function stack = read_tiff_stack(path,varargin)
-%stack = read_tiff_stack(path,downsample_ratio)
+%stack = read_tiff_stack(path,downsample_ratio,list_of_images)
+%list_of_images is a vector of indices to read out. if empty, read all.
+
+
+
+info = imfinfo(path);
+num_images = numel(info);
 
 if nargin>1
     ds = varargin{1};
+    if nargin > 2
+        k_list = varargin{2};
+    else
+        k_list = 1:num_images;
+    end
 else
     ds = 1;
 end
-info = imfinfo(path);
-num_images = numel(info);
+
 stack = zeros(info(1).Height/ds, info(1).Width/ds, num_images);
-for k = 1:num_images
+for k = k_list
     stack(:,:,k) = imresize(imread(path, k, 'Info', info),1/ds,'box');
     
 end
