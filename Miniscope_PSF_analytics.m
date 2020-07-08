3691803e2f5439602e9807afd9f06e7ad931ec5e
Nick Antipa <naantipa@gmail.com>
2020-07-07 17:20:44 -0700

    Updated PSF analytics to add MTF^-1 plots and more. This has the wrong pixel size in it!

--- Miniscope_PSF_analytics.m
+++ Miniscope_PSF_analytics.m
@@ -1,16 +1,16 @@
 %%
 pth = 'D:\Randoscope\dataforrebuttal\PSFs';
 in = load([pth,'\psf_uni_middle.mat']);
-psf_stack_uni = permute(flipud(in.psf_noaber_uni_mid_ds), [2 3 1]);
+psf_stack_uni = permute((in.psf_noaber_uni_mid_ds), [2 3 1]);
 
 
 in = load([pth,'\psf_multi_middle.mat']);
 %psf_stack_rando = permute(in.psf_montebest_ds, [2 3 1]);
-psf_stack_rando = permute(flipud(in.psf_noaber_multi_mid_ds), [2 3 1]);
+psf_stack_rando = permute((in.psf_noaber_multi_mid_ds), [2 3 1]);
 
 
 in = load([pth,'\psf_regular_middle.mat']);
-psf_stack = permute(flipud(in.psf_noaber_reg_mid_ds), [2 3 1]);
+psf_stack = permute((in.psf_noaber_reg_mid_ds), [2 3 1]);
 h6 = figure(6),clf
 h1 = figure(1),clf
 h5 = figure(5),clf
@@ -20,20 +20,41 @@ Ascore_uni = []
 ripple_reg = []
 ripple_rando = []
 ripple_uni = []
+% Designed for 3.5 micron resolution object space
+% Convert to lp/micron
+% fc = 2*NA/lambda
+% Rayleigh = 3.5 = .61 lambda/NA
+
+px = 7.3;  %Pixel size in microns/pixel in sensor space
+Mag = 5.2;   %Magnification
+px_obj = px/Mag;   %Object space microns/pixel
+[Ny,Nx] = size(psf_rando);
+R = 3.5;
+lambda = 0.51;
+NA = .61*lambda/R;
+fc_adjust = 1/2;
+fc = 2*NA/lambda*fc_adjust;   %Cycles per micron
+fmax = 1/2/px_obj;
+fgrid_rad = (0:nbins-1)/nbins*fmax;
+df_rad = mean(diff(fgrid_rad));
+fc_rad_px = fc/df_rad;
+fgrid_lin = (0:Ny/2-1)/Ny*2*fmax;
+fc_grid_px = fc/mean(diff(fgrid_lin));
 
 [X,Y] = meshgrid(-255.5:1:255.5,-255.5:1:255.5);
-c = sqrt(X.^2+Y.^2)<=100;
+c = sqrt(X.^2+Y.^2)<=(fc_grid_px+3);
 fftcorr = @(x,y)gather(real(ifft2(fft2(ifftshift(x)).*conj(fft2(y)))));
-c2 = sqrt(X.^2+Y.^2)<=70;
+c2 = sqrt(X.^2+Y.^2)<=fc_grid_px/2;
 c2 = fftcorr(c2,c2);
 
-c2 = gpuArray(single(c2/max(max(c2))));
+c2 = gpuArray(single(c2/max(max(c2))).^2);
 C2 = ifft2(c2);
 ripple_err = @(x,c)gather(norm(x - c/max(c(:))*max(x(:)),'fro'));
 Nz =72;
-Astar = @(x)c.*(1./(abs(x)+.00001));
-
-for zplane = 1:Nz
+Astar = @(x)c.*(1./(abs(x)+.00000001));
+[X,Y] = meshgrid(1:512,1:512);
+mmm = X>240 & X<275 & Y<255 & Y>230;
+for zplane = 40
     %zplane = 24
     
     psf_uni=psf_stack_uni(:,:,zplane);%6use 20 and 60
@@ -46,6 +67,10 @@ for zplane = 1:Nz
     psf_regular = psf_stack(:,:,zplane+regoffset);  %use 72 and 32
     psf_regular = psf_regular/sum(sum(psf_regular));
     
+    %This plots spectrum of one point
+    psfMask = psf_uni .* mmm;
+    spectMask = fftshift(abs(fft2(ifftshift(psfMask))));
+    [spectrad, bns] = radialavg(spectMask,nbins);
     set(0,'CurrentFigure',h1)
     clf
     subplot(1,3,1)
@@ -55,11 +80,14 @@ for zplane = 1:Nz
     title('reg')
     
     subplot(1,3,2)
-    imagesc(psf_uni)
-    axis image
-    caxis([0 .002]);
+%     imagesc(psfMask)
+%     axis image
+%     caxis([0 .002]);
+    plot(bns*fmax,spectrad)
+    hold on
+    line([fc fc],[.05 .05])
     title('uni')
-    
+    hold off
     subplot(1,3,3)
     imagesc(psf_rando)
     axis image
@@ -132,52 +160,59 @@ for zplane = 1:Nz
     end
     
     set(0,'CurrentFigure',h6)
-    semilogy(avgbins,Astar_reg)
+    semilogy(fgrid_rad,Astar_reg)
     hold on
-    semilogy(avgbins,Astar_rand)
-    semilogy(avgbins,Astar_uni)
+    semilogy(fgrid_rad,Astar_rand)
+    semilogy(fgrid_rad,Astar_uni)
+    line([fc, fc],[1 1e12])
     legend(sprintf('reg %.2g',Ascore_reg(zplane)),...
         sprintf('opt %.2g',Ascore_rand(zplane)),...
         sprintf('uni %.2g',Ascore_uni(zplane)))
     hold off
     drawnow
 end
+
+
 %%
-px = 4;  %Pixel size in microns/pixel in sensor space
-Mag = 5.2;   %Magnification
-px_obj = px/Mag;
-zvec = (1:72) * 5;
+
+zvec = (0:71) * 5;
 sm = 1;
 filt_kern = 1/sm*ones(1,sm);
 Ascore_reg_sm = filter(filt_kern,1,medfilt1(Ascore_reg,1));
 Ascore_uni_sm = filter(filt_kern,1,medfilt1(Ascore_uni,1));
 Ascore_rand_sm = filter(filt_kern,1,medfilt1(Ascore_rand,1));
+
 figure(7),clf
 
-semilogy(zvec,Ascore_reg_sm)
+plot(zvec,Ascore_reg_sm,'k-.','LineWidth',2)
 hold on
-semilogy(zvec,Ascore_uni_sm)
-semilogy(zvec,Ascore_rand_sm)
-legend('Regular','uni','design')
-title('sum(1/|MTF|')
+plot(zvec,Ascore_uni_sm,'k','LineWidth',2)
+plot(zvec,Ascore_rand_sm,'r','LineWidth',2)
+% a = gca
+% a.XTick = linspace(0
+legend('Regular unifocal','irregular unifocal','irregular multifocal (ours)')
+title('sum(1/|MTF|^2)')
 xlabel('depth \mu m')
-xlim([0 360])
+xlim([0 360-5])
+ylim([0 max(Ascore_uni_sm(:))])
 ylabel('sum(1/|MTF|)')
 hold off
+%%
 
 figure(8),clf
 
-plot(ripple_reg)
+plot(ripple_reg,'k-.','LineWidth',2)
 hold on
-plot(ripple_uni)
-plot(ripple_rando)
+plot(ripple_uni,'k','LineWidth',2)
+plot(ripple_rando,'r','LineWidth',2)
 
-legend('Regular','uni','design')
+legend('Regular unifocal','irregular unifocal','irregular multifocal (ours)')
 title('frequency space error')
 hold off
 mxc = 14;
 sc = 1;
-prep_acorr = @(x)imresize(x(:,1:mxc)./max(x,[],2),sc,'bicubic')
+px_xc = px_obj*1/sc;  %microns/pixel of scaled image
+prep_acorr = @(x)imresize(x(:,1:mxc)./max(x,[],2),sc,'bicubic');
 
 figure(9)
 clf
@@ -203,16 +238,16 @@ colormap jet
 
 temp = prep_acorr(acorrslice_reg);
  
-yplot = ((1:size(temp,2))-1)/size(temp,2)*px_obj*mxc*2  %The *2 is to make it FWHM
+yplot = ((1:size(temp,2))-1)*px_xc*2  %The *2 is to make it FWHM
 xplot = ((1:size(temp,1))-1)*72*5/size(temp,1);
 [Xplt,Yplt] = meshgrid(xplot,yplot);
-fwhm_contour = @(x,pct)contour(Xplt,Yplt,rot90(x,-1),[pct pct])
+fwhm_contour = @(x,pct,clr)contour(Xplt,Yplt,rot90(x,-1),[pct pct],clr)
 figure(10)
 clf
-fwhm_contour((prep_acorr(acorrslice_reg)),0.5);
+fwhm_contour((prep_acorr(acorrslice_reg)),0.48,'k');
 hold on
-fwhm_contour((prep_acorr(acorrslice_uni)),0.5);
-fwhm_contour((prep_acorr(acorrslice_rand)),0.5);
+fwhm_contour((prep_acorr(acorrslice_uni)),0.48,'r');
+fwhm_contour((prep_acorr(acorrslice_rand)),0.48,'b');
 
 title('FWHM vs z')
 
@@ -221,19 +256,126 @@ title('FWHM vs z')
 % acnorm_uni = prep_acorr(acorrslice_uni);
 % acnorm_rand = prep_acorr(acorrslice_rand);
 % make_fwhm = @(x,pct)sum(x>=pct,2);
-
-
-
-
 % 
-% fwhm_uni =make_fwhm(acnorm_uni,0.5)/sc/px_obj;
-% fwhm_reg = make_fwhm(acnorm_rand,0.5)/sc/px_obj;
+% 
+% 
+% 
+% 
+% fwhm_uni =make_fwhm(acnorm_uni,0.5)*px_obj*2;
+% fwhm_reg = make_fwhm(acnorm_rand,0.5)*px_obj*2;
 % plot(xplot,flipud(fwhm_uni))
 % plot(xplot,flipud(fwhm_reg))
 legend('Regular','unifocal','designed')
 hold off 
 
 %%
+
+% 
+% plot(ripple_reg,'k-.','LineWidth',2)
+% hold on
+% plot(ripple_uni,'k','LineWidth',2)
+% plot(ripple_rando,'r','LineWidth',2)
+% 
+% legend('Regular unifocal','irregular unifocal','irregular multifocal (ours)')
+
+zout = 1;
+zin = 40
+figure(13)
+clf
+
+a1 = semilogy(fgrid_rad,Astar_reg_mat(zout,:),'k-.','LineWidth',2)
+hold on
+a2 = semilogy(fgrid_rad,Astar_uni_mat(zout,:),'k','LineWidth',2)
+a3 = semilogy(fgrid_rad,Astar_rand_mat(zout,:),'r','LineWidth',2)
+%line([fc, fc],[1 1e12])
+axis([0 fc 10 max(Astar_reg_mat(zout,:))])
+legend('Regular unifocal','irregular unifocal','irregular multifocal (ours)','Location','southeast')
+title('z=0 (-200 \mu m out of focus)')
+hold off
+
+figure(14)
+clf
+semilogy(fgrid_rad,Astar_reg_mat(zin,:),'k-.','LineWidth',2)
+hold on
+semilogy(fgrid_rad,Astar_uni_mat(zin,:),'k','LineWidth',2)
+semilogy(fgrid_rad,Astar_rand_mat(zin,:),'r','LineWidth',2)
+axis([0 fc 10 max(Astar_reg_mat(zout,:))])
+legend('Regular unifocal','irregular unifocal','irregular multifocal (ours)','Location','southeast')
+title('z=200 \mu m (In focus)')
+
+hold off
+
+% Same thing but linear scale
+figure(15)
+clf
+
+a1 = plot(fgrid_rad,Astar_reg_mat(zout,:),'k-.','LineWidth',2)
+hold on
+a2 = plot(fgrid_rad,Astar_uni_mat(zout,:),'k','LineWidth',2)
+a3 = plot(fgrid_rad,Astar_rand_mat(zout,:),'r','LineWidth',2)
+%line([fc, fc],[1 1e12])
+axis([0 fc 10 max(Astar_reg_mat(zout,:))])
+legend('Regular unifocal','irregular unifocal','irregular multifocal (ours)','Location','northwest')
+title('z=0 (-200 \mu m out of focus)')
+hold off
+
+figure(16)
+clf
+plot(fgrid_rad,Astar_reg_mat(zin,:),'k-.','LineWidth',2)
+hold on
+plot(fgrid_rad,Astar_uni_mat(zin,:),'k','LineWidth',2)
+plot(fgrid_rad,Astar_rand_mat(zin,:),'r','LineWidth',2)
+axis([0 fc 10 max(Astar_reg_mat(zout,:))])
+legend('Regular unifocal','irregular unifocal','irregular multifocal (ours)','Location','northwest')
+title('z=200 \mu m (In focus)')
+
+hold off
+%%
+outpth = 'D:\\Randoscope\\dataforrebuttal\\PSFs'
+vec = @(x)x(:);
+imnorm = max([max(vec(psf_stack(:,:,zin))) max(vec(psf_stack_uni(:,:,zin))) max(vec(psf_stack_rando(:,:,zin)))])
+cmap = double(imread(['D:\Randoscope\dataforrebuttal\cmap_imagej_fire.tif']))/255
+ncolors = length(colormap(cmap));
+r1 = 80;
+r2 = 512-100;
+c1 = 90;
+c2 = 512-90;
+make_outim = @(im,cmap,nm)ind2rgb(gray2ind(uint8(im(r1:r2,c1:c2)/nm*255),length(colormap(cmap))),colormap(cmap));
+rando_in = make_outim(psf_stack_rando(:,:,zin),cmap,imnorm);
+reg_in = make_outim(psf_stack(:,:,zin),cmap,imnorm);
+uni_in = make_outim(psf_stack_uni(:,:,zin),cmap,imnorm);
+reg_out = make_outim(psf_stack(:,:,zout),cmap,imnorm);
+uni_out = make_outim(psf_stack_uni(:,:,zout),cmap,imnorm);
+rando_out = make_outim(psf_stack_rando(:,:,zout),cmap,imnorm);
+
+imwrite(rando_in,[outpth,'\rando_in.png'])
+imwrite(reg_in,[outpth,'\regular_in.png'])
+imwrite(uni_in,[outpth,'\uni_in.png'])
+imwrite(rando_out,[outpth,'\rando_out.png'])
+imwrite(reg_out,[outpth,'\reg_out.png'])
+imwrite(uni_out,[outpth,'\uni_out.png'])
+imshow(uni_out)
+%%
+surfs_in = load(['D:\Randoscope\dataforrebuttal\PSFs\surfaces_with_apreture.mat'])
+optsurf_in = load('D:\Randoscope\dataforrebuttal\PSFs\optimized_mask');
+smax = 4e-3;
+px_srf = 3600/2400; 
+r1 = 512-2*(size(reg_out,1)/2)
+r2 = 512+2*(size(reg_out,1)/2)
+c1 = r1
+c2 = r2
+make_outim = @(im,cmap,nm)ind2rgb(gray2ind(uint8(im(r1:r2,c1:c2)/nm*255),length(colormap(cmap))),colormap(cmap));
+surfreg = make_outim(imresize(surfs_in.Tnoaber_reg_mid,[1024,1024]),'parula',smax);
+surfuni = make_outim(imresize(surfs_in.Tnoaber_uni_mid,[1024,1024]),'parula',smax);
+surfrand = make_outim(imresize(surfs_in.Tnoaber_multi_mid,[1024,1024]),'parula',smax);
+surfopt = make_outim(imresize(optsurf_in.Taber_multi,[1024,1024]),'parula',smax);
+imshow(surfrand)
+
+imwrite(surfreg,[outpth,sprintf('\\regular_surface_%g_um_FoV_cmax_%g_um.png',(r2-r1)*px_srf*2,smax*1e3)])
+imwrite(surfuni,[outpth,sprintf('\\unifocal_surface_%g_um_FoV_cmax_%g_um.png',(r2-r1)*px_srf*2,smax*1e3)])
+imwrite(surfrand,[outpth,sprintf('\\multifocal_surface_%g_um_FoV_cmax_%g_um.png',(r2-r1)*px_srf*2,smax*1e3)])
+imwrite(surfopt,[outpth,sprintf('\\optimized_surface_%g_um_FoV_cmax_%g_um.png',(r2-r1)*px_srf*2,smax*1e3)])
+
 
 %% Make axial correlation plots for resolution analysis
 
